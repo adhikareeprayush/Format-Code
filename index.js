7108db7c26f9fd51b53e2825f6392435612c1d2c
Prayush Adhikari <115662438+adhikareeprayush@users.noreply.github.com>
2024-08-09 11:56:53 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-function helloWorld() {
-  console.log('Hello, world!');
+function
+  helloWorld
+  () 
+
+
+
+{
+  console.log('
+              Hello, world!
+    ');
+
+
+  
 }
 
-helloWorld();
+helloWorld
+  (
+    
+  )
+  ;
